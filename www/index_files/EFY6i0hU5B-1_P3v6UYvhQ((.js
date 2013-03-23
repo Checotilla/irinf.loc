34a40440baae88a00dfc78e58679805f4ca85951
Valentin Kosarev <astitstudio@gmail.com>
2013-03-24 01:51:14 +0400

    продолжается верстка главной страницы
    доделан блок соц-сетей и начат футер

--- www/index_files/EFY6i0hU5B-1_P3v6UYvhQ((.js
+++
@@ -1,2 +0,0 @@
-
-Bootstrapper.bindDOMParsed(function(){try{Bootstrapper.setCurrentRuleId(66575);function bk_idswap(){var m=document.cookie.match(/MUID=(.*?);/);var a=document.cookie.match(/ANON=A=(.*?)&/);if(m!==null){if(m.length>0){m=m[1];}var i=new Image();var src='';if(window.location.protocol==="https:"){src="https://stags.bluekai.com/site/13157?id="+m;}else{src="http://tags.bluekai.com/site/13157?id="+m;}if(a!==null){if(a.length>0){a=a[1];}src+="&phint=anid%3D"+a;}i.src=src;}}bk_idswap();;}catch(e){Bootstrapper.reportException(e);}});;
--- www/index_files/serverComponent.php
+++
@@ -1,1 +0,0 @@
-var psj0 = 'https://nexus.ensighten.com/mscom/code/EFY6i0hU5B-1_P3v6UYvhQ((.js?ruleId=66575';Bootstrapper.loadScriptCallback(psj0, Bootstrapper.callOnPageSpecificCompletion); 
